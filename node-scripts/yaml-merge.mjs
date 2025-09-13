3a76139b5b5c41c3d6cf7321d7a8b9d8681b9a2a
zakzakst <e1433104@gmai.com>
2025-09-13 21:25:35 +0900

    fix

---
+++ node-scripts/yaml-merge.mjs
@@ -0,0 +1,16 @@
+import fs from "fs";
+import yaml from "js-yaml";
+import merge from "lodash.merge";
+
+function loadYaml(filePath) {
+  const content = fs.readFileSync(filePath, "utf8");
+  return yaml.load(content);
+}
+
+const file1 = loadYaml("openapi/todos.yaml");
+const file2 = loadYaml("openapi/users.yaml");
+
+const merged = merge({}, file1, file2);
+
+fs.writeFileSync("openapi/_bundle.yaml", yaml.dump(merged), "utf8");
+console.log("_bundle.yamlを生成しました");
