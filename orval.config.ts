6db5857fdd4c2016a654e899a23a8ca1d4643865
zakzakst <e1433104@gmai.com>
2025-09-16 01:37:07 +0900

    fix

--- orval.config.ts
+++ orval.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "orval";
 // https://github.com/orval-labs/orval/blob/master/samples/react-query/custom-fetch/src/custom-fetch.ts
 
 export default defineConfig({
-  usersApi: {
+  users: {
     input: "./openapi/users.yaml",
     output: {
       target: "./src/orval/users.ts",
@@ -19,10 +19,24 @@ export default defineConfig({
       afterAllFilesWrite: "npm run lint",
     },
   },
-  todosApi: {
+  todos: {
     input: "./openapi/todos.yaml",
     output: {
       target: "./src/orval/todos.ts",
+      client: "swr",
+      httpClient: "fetch",
+      baseUrl: "/api",
+      mock: true,
+      // clean: true,
+    },
+    hooks: {
+      afterAllFilesWrite: "npm run lint",
+    },
+  },
+  schedules: {
+    input: "./openapi/schedules.yaml",
+    output: {
+      target: "./src/orval/schedules.ts",
       client: "swr",
       httpClient: "fetch",
       baseUrl: "/api",
--- src/lib/msw/handlers/browser.ts
+++ src/lib/msw/handlers/browser.ts
@@ -1,10 +1,12 @@
 "use client";
 
 import type { RequestHandler } from "msw";
-import { getTodosAPIMock } from "@/orval/todos";
-import { getUsersAPIMock } from "@/orval/users";
+import { getApiMock as getTodosApiMock } from "@/orval/todos";
+import { getApiMock as getUsersApiMock } from "@/orval/users";
+import { getApiMock as getSchedulesApiMock } from "@/orval/schedules";
 
 export const handlers: RequestHandler[] = [
-  ...getTodosAPIMock(),
-  ...getUsersAPIMock(),
+  ...getTodosApiMock(),
+  ...getUsersApiMock(),
+  ...getSchedulesApiMock(),
 ];
