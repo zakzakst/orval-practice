2ec8f426a7dbdf8be81f7fdd2e4774aee5b55693
zakzakst <e1433104@gmai.com>
2025-09-25 01:47:41 +0900

    fix

--- openapi/mock-fetch/schedules.ts
+++ openapi/mock-fetch/schedules.ts
@@ -1,8 +1,23 @@
+import { AxiosResponse } from "axios";
+
+// export const getSchedulesMockFetcher = <T>(
+//   url: string,
+//   options?: RequestInit
+// ): Promise<T> => {
+//   console.log(`get: ${url}`);
+//   return Promise.resolve({ message: "getSchedulesMockFetcher成功" } as T);
+//   return Promise.reject({ message: "getSchedulesMockFetcher失敗" } as T);
+// };
+
 export const getSchedulesMockFetcher = <T>(
   url: string,
   options?: RequestInit
-): Promise<T> => {
+): Promise<AxiosResponse<T>> => {
   console.log(`get: ${url}`);
-  return Promise.resolve({ message: "getSchedulesMockFetcher成功" } as T);
-  return Promise.reject({ message: "getSchedulesMockFetcher失敗" } as T);
+  return Promise.resolve({
+    data: { message: "getSchedulesMockFetcher成功" },
+  } as AxiosResponse<T>);
+  return Promise.reject({
+    data: { message: "getSchedulesMockFetcher失敗" },
+  } as AxiosResponse<T>);
 };
