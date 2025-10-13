467c0cc9a283e0e3d0a6259cb1d197d378720c64
zakzakst <e1433104@gmail.com>
2025-10-14 08:30:07 +0900

    fix

--- src/app/zod4/page.tsx
+++ src/app/zod4/page.tsx
@@ -1,6 +1,6 @@
 // import { Form } from "./components/Form1";
-// import { Form } from "./components/Form2";
-import { Form } from "./components/Form3";
+import { Form } from "./components/Form2";
+// import { Form } from "./components/Form3";
 
 const Page = () => {
   return (
